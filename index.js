56a2baba50ec848f4c66634ea0dcd7fa4390e0b1
rerxyz_ <reynard16rande@gmail.com>
2026-07-10 14:27:57 +0800

    fix: cors link

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://taskmanager-rer.netlify.app/'] }));
 app.use(express.json());
 
 app.use('/tasks', tasksRoutes);
